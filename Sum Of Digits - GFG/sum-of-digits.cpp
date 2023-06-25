6ffcc847dea1680ced60ada2443a86511ca9b979
Tanvi Jain <136319021+tanvi-jain-1@users.noreply.github.com>
2023-06-25 12:52:21 +0530

    Added solution - LeetHub

---
+++ Sum Of Digits - GFG/sum-of-digits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int sumOfDigits(int N){
+        int k=N;
+        
+        int sum=0;
+        while(k>0)
+        {
+            int last_digit=k%10;
+            k=k/10;
+            sum+=last_digit;
+        }
+        return sum;
+        //code here
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.sumOfDigits(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
